9ffa0f6db4d75bd477507d65bf7e381b3150b766
nandkishore-patidar <119879956+nandkishore-patidar@users.noreply.github.com>
2023-01-03 19:09:35 +0530

    udpated files

---
+++ src/main/java/com/maven/release/maven_release_testing/App.java
@@ -0,0 +1,13 @@
+package com.maven.release.maven_release_testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/maven/release/maven_release_testing/AppTest.java
@@ -0,0 +1,20 @@
+package com.maven.release.maven_release_testing;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
